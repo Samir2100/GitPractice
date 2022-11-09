f6614083cb34f5133d4bd547f7b8d27dc2fbe3b4
Samir2100 <samir.timory@tekschool.us>
2022-11-08 20:14:58 -0500

    this is from local repo

--- src/gitPractice/TestGitcommit.java
+++ src/gitPractice/TestGitcommit.java
@@ -1,5 +1,11 @@
 package gitPractice;
 
 public class TestGitcommit {
+	public static void main(String[] args) {
+		
+	}
 
+	public void test() {
+		System.out.println("this is from local repo");
+	}
 }
